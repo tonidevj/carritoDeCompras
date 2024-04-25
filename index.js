ea9d45a1d18b8e125dca6f07fb4cd669419af467
Antoni Bogado <153125397+tonidevj@users.noreply.github.com>
2024-04-24 20:56:44 -0400

    Update index.js
    no funciono en android

--- index.js
+++ index.js
@@ -41,34 +41,3 @@ shopIcon.addEventListener('click', e => {
 tableClear.addEventListener('click', e =>{
     table.innerHTML = '';
 });
-
-courseBtn.forEach(btn => {
-    btn.addEventListener('touchend', e => { // Cambio el click por touchend
-        const img = e.target.parentElement.parentElement.children[0].innerHTML;
-        const name = e.target.parentElement.children[0].innerHTML;
-        
-        const exist = [...table.children].find(element => element.children[1].innerHTML === name);
-        if (exist) {
-            exist.children[3].innerHTML = Number(exist.children[3].innerHTML) + 1;
-        } else {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${img}</td>
-                <td>${name}</td>
-                <td>15$</td>
-                <td>1</td>
-                <td>
-                    <svg class="delete-btn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                </td>
-            `;
-            row.children[4].addEventListener('click', e => {
-                e.currentTarget.parentElement.remove();
-            });
-            
-            table.append(row);
-        }
-    });
-});
-
